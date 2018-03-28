bb91a0e7b77888724ac9ef2f6603567c8b15c7db
Scott McKenzie <smckenzie@8i.com>
2018-03-28 19:11:38 +1000

    Fix number entry - Test if last string is a number and append string if it is

--- ios/calculator/Model/Engine.swift
+++ ios/calculator/Model/Engine.swift
@@ -68,7 +68,7 @@ final class Engine {
             
         case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
             
-            if let op = inputs.last, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].contains(op) {
+            if let op = inputs.last, let _ = Double(op) {
                 
                 let value = "\(op)\(input)"
 
